afe317927735d2af74be8cf097437f641b226fd2
Molly Kuhs <kuhsx003@umn.edu>
2024-02-11 11:02:46 -0600

    update figs

--- Week 5/week5.R
+++ Week 5/week5.R
@@ -23,7 +23,7 @@ p<- ggplot(data, aes(col, Percentage, fill = Category))+
            linewidth = 0.1)+
   theme_void()+
   scale_fill_manual(values = c("#1e1e1e", "#42271b", "#fdbb00"))+
-  coord_cartesian(y = (c(4, 102)), clip = "off")+
+  coord_cartesian(y = (c(4, 103.5)), clip = "off")+
   xlim(c(-6, 5))+
   theme(
     legend.position = "none",
@@ -47,7 +47,7 @@ pLabs<- p +
         plot.caption = element_text(family = "Vasarely-Light",
                                     size = 7.5,
                                     hjust = 0.5))+
-  annotate("text", x= -0.5, y= 105.5, label = subtitle,
+  annotate("text", x= -0.5, y= 107.15, label = subtitle,
           family = "Vasarely-Light",
           size = 3.1,
           hjust = 0.5,
@@ -60,20 +60,20 @@ pLabs +
   annotate("text", x =1 , y = 78, 
            label = "44%",
            family = "B52-ULCW00-ULC",
-           size = 4.5,
+           size = 4,
            hjust = 0.5,
            color = "#e5d6c3")+
   annotate("text", x =1 , y = 36, 
            label = "40%",
            family = "B52-ULCW00-ULC",
-           size = 4.5,
+           size = 4,
            hjust = 0.5,
            color = "#9b0826",
            alpha = 0.4)+
   annotate("text", x =1 , y = 8, 
            label = "16%",
            family = "B52-ULCW00-ULC",
-           size = 4.5,
+           size = 4,
            hjust = 0.5,
            color = "#38271f")+
   
@@ -81,19 +81,19 @@ pLabs +
   annotate("text", x =-4.6 , y = 94, 
            label = "BLACK.",
            family = "B52-ULCW00-ULC",
-           size = 5.2,
+           size = 4.8,
            hjust = 0,
            color = "#38271f")+
   annotate("text", x =-4.6 , y = 49, 
            label = "BROWN.",
            family = "B52-ULCW00-ULC",
-           size = 5.2,
+           size = 4.8,
            hjust = 0,
            color = "#38271f")+
   annotate("text", x =-4.6 , y = 10.2, 
            label = "YELLOW.",
            family = "B52-ULCW00-ULC",
-           size = 5.2,
+           size = 4.8,
            hjust = 0,
            color = "#38271f")+
  
@@ -123,4 +123,9 @@ pLabs +
 
 #final 
 ggsave("week5.png", width = 5.65, height = 7, units = "in") 
+
+
+
+##INCREASE Y AXIS 
+##MESS WITH TEXT SIZE
 
